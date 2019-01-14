85419969797f40a21b47109d62761b1b1a985889
sjaces <sjaces@gmail.com>
2019-01-14 17:00:07 +0000

    Importing react components

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hola mundo!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+//console.log("Hola mundo!");
+
+const app = document.getElementById('app');
+const holamundo = <h1>Hola mundo!</h1>;
+
+// ReactDOM.render(lo que quiero renderizar, dónde lo quiero renderizar);
+
+ReactDOM.render(holamundo, app);
